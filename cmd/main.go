544cdb2cf988b6e8e6c524edec533bae09c370d5
datslim <78911378+datslim@users.noreply.github.com>
2025-05-29 14:55:46 +0300

    feat: GET quotes by author && fix: randomQuote

--- cmd/main.go
+++ cmd/main.go
@@ -25,11 +25,25 @@ func handleRequests() {
 	router.HandleFunc("/quotes", postQuote).Methods("POST")
 	router.HandleFunc("/quotes", getAllQuotes).Methods("GET")
 	router.HandleFunc("/quotes/random", getRandomQuote).Methods("GET")
+	router.HandleFunc("/quotes", deleteQuote).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func getAllQuotes(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(UserDatas)
+	author := r.URL.Query().Get("author")
+
+	if author == "" {
+		json.NewEncoder(w).Encode(UserDatas)
+	}
+
+	var filtredQuotes []UserData
+	for _, quote := range UserDatas {
+		if quote.Author == author {
+			filtredQuotes = append(filtredQuotes, quote)
+		}
+	}
+
+	json.NewEncoder(w).Encode(filtredQuotes)
 }
 
 func postQuote(w http.ResponseWriter, r *http.Request) {
@@ -45,6 +59,14 @@ func postQuote(w http.ResponseWriter, r *http.Request) {
 }
 
 func getRandomQuote(w http.ResponseWriter, r *http.Request) {
+	if len(UserDatas) == 0 {
+		http.Error(w, "No quotes available", http.StatusNotFound)
+		return
+	}
 	randomNumber := rand.Intn(len(UserDatas))
 	json.NewEncoder(w).Encode(UserDatas[randomNumber])
+}
+
+func deleteQuote(w http.ResponseWriter, r *http.Request) {
+
 }
