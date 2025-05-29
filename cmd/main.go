edd6555e23b98abb495f895827202b3d6862215d
datslim <78911378+datslim@users.noreply.github.com>
2025-05-29 15:08:30 +0300

    fix: replace ID logic for more convenient

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type UserData struct {
+	ID     int    `json:"id"`
 	Author string `json:"author"`
 	Quote  string `json:"quote"`
 }
@@ -25,7 +26,7 @@ func handleRequests() {
 	router.HandleFunc("/quotes", postQuote).Methods("POST")
 	router.HandleFunc("/quotes", getAllQuotes).Methods("GET")
 	router.HandleFunc("/quotes/random", getRandomQuote).Methods("GET")
-	router.HandleFunc("/quotes", deleteQuote).Methods("DELETE")
+	router.HandleFunc("/quotes/{id}", deleteQuote).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
@@ -48,14 +49,22 @@ func getAllQuotes(w http.ResponseWriter, r *http.Request) {
 
 func postQuote(w http.ResponseWriter, r *http.Request) {
 	var newData UserData
+	var currentID int = 0
 	err := json.NewDecoder(r.Body).Decode(&newData)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	if len(UserDatas) == 0 {
+		currentID = 1
+	} else {
+		currentID = len(UserDatas) + 1
+	}
+	newData.ID = currentID
 	UserDatas = append(UserDatas, newData)
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newData)
 }
 
 func getRandomQuote(w http.ResponseWriter, r *http.Request) {
